0df09868932f933d121f8540a10838aecbc7a279
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-10-10 22:34:50 -0300

    feat: complete requirement 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for data in instance.data:
+        occurences = []
+        results.append({
+            'palavra': word,
+            'arquivo': data['nome_do_arquivo'],
+            'ocorrencias': occurences
+        })
+        for index, line in enumerate(data['linhas_do_arquivo']):
+            if word in line:
+                occurences.append({'linha': index + 1})
+        if not len(occurences):
+            results.pop()
+
+    return results
 
 
 def search_by_word(word, instance):
